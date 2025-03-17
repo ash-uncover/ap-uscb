e745e06327636adc6ac42d40e419bb7ca35b3b36
ash-uncover <ash.uncover@gmail.com>
2025-03-17 08:40:28 +0100

    final stats

--- src/components/players/player/Player.tsx
+++ src/components/players/player/Player.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
@@ -16,6 +16,13 @@ export const Player = () => {
   // #region Hooks
   const playerId = useParams().playerId
   const player = useSelector(DataSelectors.player(playerId))
+  const matchs = useSelector(DataSelectors.matchs)
+  const [playerMatchs, setPlayerMatchs] = useState([])
+  useEffect(() => {
+    setPlayerMatchs(Object.values(matchs).filter(
+      match => match.players.some(p => p.player === playerId)
+    ))
+  }, [playerId, matchs])
   // #endregion
 
   // #region Callbacks
@@ -23,7 +30,6 @@ export const Player = () => {
 
   // #region Rendering
   const classes = ['ap-player']
-
   return (
     <div className={classes.join(' ')}>
       <Title
@@ -31,6 +37,13 @@ export const Player = () => {
         size={TitleLevels.H1}
       >
         {playerId}
+      </Title>
+
+      <Title
+        level={TitleLevels.H2}
+        size={TitleLevels.H2}
+      >
+        Stats Joueur
       </Title>
       <div className='ap-player-tiles ap-tiles'>
         <Tile
@@ -60,6 +73,11 @@ export const Player = () => {
           icon={['fas', 'basketball']}
         />
         <Tile
+          text={`${player.points2e + player.points2i}`}
+          desc='2 Points'
+          icon={['fas', 'basketball']}
+        />
+        <Tile
           text={`${player.pointsPerMatch.toFixed(2)}`}
           desc='Points par match'
           size={TileSizes.M}
@@ -72,6 +90,35 @@ export const Player = () => {
           icon={['fas', 'clock']}
         />
       </div>
+
+      <Title
+        level={TitleLevels.H2}
+        size={TitleLevels.H2}
+      >
+        Matchs Joués
+      </Title>
+      <table className={'ap-table'}>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temps de jeu</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {playerMatchs.map((match) => {
+            const playerMatchData = match.players.find(p => p.player === playerId)!
+            return (
+              <tr key={match.date}>
+                <td>{match.date}</td>
+                <td>{timeToString(playerMatchData.time)}</td>
+                <td>{playerMatchData.points}</td>
+              </tr>
+            )
+          }
+          )}
+        </tbody>
+      </table>
     </div>
   )
   // #endregion
