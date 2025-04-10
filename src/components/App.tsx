c122f599e3cba6d69769afb96ed5efff56afb1b7
ash-uncover <ash.uncover@gmail.com>
2025-04-10 18:50:00 +0200

    smooth header scroll

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, useLocation } from 'react-router-dom'
 import { DataStates } from '@sol.ac/js-utils'
@@ -17,9 +17,11 @@ export const App = () => {
   // #region Hooks
   const dispatch = useDispatch()
   const ref = useRef(null)
+  const refScroll = useRef(0)
   const dataState = useSelector(DataSelectors.dataState)
   const dataError = useSelector(DataSelectors.dataError)
   const location = useLocation()
+  const [scrolling, setScrolling] = useState(false)
   useEffect(() => {
     if (dataState === DataStates.NEVER) {
       const params = new URLSearchParams(location.search)
@@ -29,13 +31,27 @@ export const App = () => {
   useEffect(() => {
     if (ref.current) {
       ref.current.scrollTop = 0
+      refScroll.current = 0
+      setScrolling(false)  
     }
   }, [location])
+  // #endregion
+
+  // #region Callbacks
+  function handleScrollMain(e: any) {
+    if (e.target.scrollTop > 40) {
+      const scrolling = refScroll.current < e.target.scrollTop
+      setScrolling(scrolling)
+    }
+    refScroll.current = e.target.scrollTop
+  }
   // #endregion
   
   // #region Rendering
   const classes = ['ap-app']
-
+  if (scrolling) {
+    classes.push('ap-app--scrolled')
+  }
   switch (dataState) {
     case DataStates.NEVER:
     case DataStates.FETCHING:
@@ -60,7 +76,11 @@ export const App = () => {
       return (
         <div className={classes.join(' ')}>
           <AppHeader />
-          <main className='ap-app-main' ref={ref}>
+          <main 
+            onScroll={handleScrollMain}
+            className='ap-app-main' 
+            ref={ref}
+          >
             <Outlet />
           </main>
           <AppNav />
