bd46df6c2c08718f7eb9583c2ddbbd192c8e79ad
ash-uncover <ash.uncover@gmail.com>
2026-01-17 10:01:55 +0100

    add match

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,12 +10,9 @@ export const DATA: DataEntry[] = [{
   file: 'data_uscb2-2024-2025',
   text: 'SM2 - 2024/2025'
 }, {
-  file: 'data_uscb1-2025-2026',
-  text: 'SM1 - 2025/2026'
-}, {
   file: 'data_uscb1-2024-2025',
   text: 'SM1 - 2024/2025'
 }, {
   file: 'data_uscbu13-2025-2026',
-  text: 'U13 - 2024/2025'
+  text: 'U13 - 2025/2026'
 }]
